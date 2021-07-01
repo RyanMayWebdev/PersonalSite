a9a2530e65608826966495e5354283556c00bfa4
Ryan May <ryan777may@gmail.com>
2021-06-30 21:49:13 -0400

    Update styles for mobile

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -30,7 +30,7 @@ function mobileMenuToggle() {
 const observerOptions = {
     root: null,
     rootMargin: "0px",
-    threshold: 0.3
+    threshold: 0.1
 };
 
 function observerCallback(entries) {
